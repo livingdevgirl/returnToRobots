0a6e62dbfcd1fd072e1acebc43ca3817117ca32c
Emma Dean <emmadeannow@gmail.com>
2017-08-09 06:39:18 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const dataFile = require('./public/data');
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./db');
+let url = 'mongodb://localhost:3000/robotsMongo';
+
+
+app.engine('handlebars', expressHandlebars());
+app.set('views', './views');
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+//for loop if value key is null, target same place and assign it to string "availble for hire" console.log(dataFile)
+for (var i = 0; i < dataFile.users.length; i++) {
+  if (dataFile.users[i].job === null){
+        let userStatus = "looking for work";
+        dataFile.users[i].job = "reboot me, i need work!";
+        // document.querySelector('.job').style.color = "red";
+  }
+}
+  res.render('home', dataFile)
+});
+
+
+app.get("/:user", function(req, res){
+  let robot = req.params.user;
+  for (var i = 0; i < dataFile.users.length; i++) {
+    if(dataFile.users[i].username === robot){
+    res.render('user', dataFile.users[i]);
+    }
+  }
+});
+
+db.connect(url, (err, connection) => {
+  if (!err)
+    console.log('connected to mongo')});
+
+app.listen(3000);
+console.log("listening at 3000");
